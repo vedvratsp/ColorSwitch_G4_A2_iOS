2b160580bfbe927ea1d050f57e00bacd08de4012
vedvrat <vedvratsp@gmail.com>
2020-02-18 03:48:31 -0500

    Internal documentation added.

--- SwitchTheShitOutOfTheColor/Other/Strings.swift
+++ SwitchTheShitOutOfTheColor/Other/Strings.swift
@@ -5,6 +5,12 @@
 //  Created by Group4 on 25/2/20.
 //  Copyright © 2020 Group4. All rights reserved.
 //
+// Team:
+// (301090878) Vishal Chanchlani
+// (301089005) Vedvrat Singh Parihar
+// (301094688) Nisha Tandel
+// Assignment 2 : Arcade Game
+// Version 1.0
 
 import Foundation
 
